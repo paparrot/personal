2cad18c28b8f9be8b9338e1dc7b39862cb2b805c
Klim <subbotin.klim@gmail.com>
2023-07-29 17:57:37 +0200

    update footer data

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,5 +1,10 @@
 <footer class="footer footer-center p-4 bg-base-300 text-base-content">
     <div>
-        <p>Copyright © 2023 - All right reserved by ACME Industries Ltd</p>
+        <p>Copyright © 2020-{{ now()->year }} - Build with Laravel and ❤️ by
+            <a
+                href="https://github.com/paparrot"
+                target="_blank"
+                rel="noopener noreferrer"
+            >@paparrot</a></p>
     </div>
 </footer>
